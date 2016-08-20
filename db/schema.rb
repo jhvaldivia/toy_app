4cfc256827fa47777c459e84172c0148e7a48eca
Jaime Valdivia <jaivaldivia@gmail.com>
2016-08-20 15:41:40 -0500

    DB Migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160820201740) do
+ActiveRecord::Schema.define(version: 20160820204010) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text     "content"
+    t.string   "user_id"
+    t.string   "integer"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
